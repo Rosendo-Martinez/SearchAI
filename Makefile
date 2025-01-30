b8db352f5235cc2ba2d0f73df8db1d9bc98d81e4
Rosendo Martinez <rosendomartinez270@gmail.com>
2025-01-30 13:57:56 -0600

    Hello window!

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 ./bin/main.exe : ./src/main.cpp
-	g++ -o ./bin/main.exe ./src/main.cpp
+	g++ -o ./bin/main.exe ./src/main.cpp ./dep/glad/src/glad.c -I./dep/glad/include -lglfw -ldl
 
 run : ./bin/main.exe
 	./bin/main.exe
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,71 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+
+bool initialize(GLFWwindow* &window, unsigned int width, unsigned int height);
+void processInput(GLFWwindow *window);
 
 int main()
 {
-    std::cout << "Hello A1." << std::endl;
-    
+    // Window
+    GLFWwindow* window = nullptr;
+    const unsigned int SCR_WIDTH = 1280;
+    const unsigned int SCR_HEIGHT = 720;
+
+    if (!initialize(window, SCR_WIDTH, SCR_HEIGHT))
+    {
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+/**
+ * Initializes window and GLAD.
+ * 
+ * Returns false if something went wrong, and logs error.
+ */
+bool initialize(GLFWwindow* &window, unsigned int width, unsigned int height)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(width, height, "A1", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return false;
+    }    
+    glViewport(0, 0, width, height);
+
+    return true;
 }
