ebc4cadb1a270b635405ce7d30f4ebb0ac00e03d
Rosendo Martinez <rosendomartinez270@gmail.com>
2025-02-03 19:18:08 -0600

    Clean up

--- src/SearchAI.cpp
+++ src/SearchAI.cpp
@@ -95,112 +95,6 @@ bool isValidAction(GridCell state, Action act, const std::vector<Node*>& closed,
     return true;
 }
 
-// std::vector<GridCell> searchBFS(GridCell start, GridCell end, const Grid& grid)
-// {
-
-//     std::vector<Node*> closed;
-//     std::queue<Node*> open;
-
-//     Node* initial = new Node;
-//     initial->parent = nullptr;
-//     initial->state = start;
-//     open.push(initial);
-//     initial = nullptr;
-
-//     // WARNING: make sure to clean up nodes in heap
-
-//     while (true)
-//     {
-//         if (open.empty()) // no solution
-//         {
-//             // Free memory 
-//             for (Node* &n : closed)
-//             {
-//                 delete n;
-//                 n = nullptr;
-//             }
-//             while (!open.empty())
-//             {
-//                 Node* &n = open.front();
-//                 open.pop();
-
-//                 delete n;
-//                 n = nullptr;
-//             }
-
-//             std::vector<GridCell> empty;
-//             return empty;
-//         }
-
-//         Node* n = open.front();
-//         open.pop();
-
-//         if (n->state.row == end.row && n->state.col == end.col) // at goal
-//         {
-//             // create solution list
-//             std::vector<GridCell> solution;
-//             Node* cur = n;
-//             while (cur != nullptr)
-//             {
-//                 solution.push_back(cur->state);
-//                 cur = cur->parent;
-//             }
-
-//             // Free memory
-//             for (Node* &n : closed)
-//             {
-//                 delete n;
-//                 n = nullptr;
-//             }
-//             while (!open.empty())
-//             {
-//                 Node* &n = open.front();
-//                 open.pop();
-
-//                 delete n;
-//                 n = nullptr;
-//             }
-//             delete n;
-//             n = nullptr;
-
-//             return solution;
-//         }
-
-//         // Expand node
-
-//         closed.push_back(n);
-//         if (isValidAction(n->state, UP, closed, grid))
-//         {
-//             Node* child = new Node;
-//             child->state = doAction(n->state, UP);
-//             child->parent = n;
-//             open.push(child);
-//         }
-//         if (isValidAction(n->state, DOWN, closed, grid))
-//         {
-//             Node* child = new Node;
-//             child->state = doAction(n->state, DOWN);
-//             child->parent = n;
-//             open.push(child);
-//         }
-//         if (isValidAction(n->state, LEFT, closed, grid))
-//         {
-//             Node* child = new Node;
-//             child->state = doAction(n->state, LEFT);
-//             child->parent = n;
-//             open.push(child);
-//         }
-//         if (isValidAction(n->state, RIGHT, closed, grid))
-//         {
-//             Node* child = new Node;
-//             child->state = doAction(n->state, RIGHT);
-//             child->parent = n;
-//             open.push(child);
-//         }
-//     }
-// }
-
-
 void SearchAI::init(GridCell start, GridCell end, Grid* grid)
 {
     // Free memory
@@ -234,7 +128,7 @@ void SearchAI::step()
 
     if (this->open.empty() || this->foundGoal) // no solution / done
     {
-        std::cout << "Open Empty or Found Goal\n";
+        std::cout << "  Open Empty or Found Goal\n";
         return;
     }
 
@@ -242,7 +136,7 @@ void SearchAI::step()
 
     if (n->state.row == this->end.row && n->state.col == this->end.col) // at goal
     {
-        std::cout << "At goal\n";
+        std::cout << "  At goal\n";
         this->foundGoal = true;
         return;
     }
